6034177a9fff8f3d00b6099147fceaecf8361145
rajkapoor7 <mail2rajkapoor@gmail.com>
2019-12-29 02:42:24 +0530

    My First Push for Jenkins

---
+++ newjenkinsproject/src/newjenkinspkg/Newjenkinscode.java
@@ -0,0 +1,14 @@
+package newjenkinspkg;
+
+public class Newjenkinscode {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Raj sample GitCode");
+
+	}
+
+}
