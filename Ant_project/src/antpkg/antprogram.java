0a487333fd8a276cf6ba92c8993afb22de5a63cc
nrajkapoor7 <mail2rajkapoor@gmail.com>
2020-01-11 01:02:46 +0530

    Ant code

--- Ant_project/src/antpkg/antprogram.java
+++ Ant_project/src/antpkg/antprogram.java
@@ -4,7 +4,7 @@ public class antprogram {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("ant sample code");
+		System.out.println("ant sample codesS");
 
 	}
 
